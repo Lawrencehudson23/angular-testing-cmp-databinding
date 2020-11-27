11613bbfcb3cb2a82adb509cceb70b313eb5d7de
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-11-26 17:23:42 -0800

    feat: sub 2 way binding(NgModel) to local references and @ViewChild

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -16,22 +23,30 @@ export class CockpitComponent implements OnInit {
     serverName: string;
     serverContent: string;
   }>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  // NOTE: @ViewChild
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
   constructor() {}
 
   ngOnInit(): void {}
-  onAddServer() {
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(
+      'this.serverContentInput: ',
+      this.serverContentInput.nativeElement.value
+    );
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent,
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
